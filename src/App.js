2d4bad9527efcd5351159c994e64d0df85c3a91c
dbrothers <dbrothers@ruvos.com>
2023-12-13 14:10:22 -0500

    Removed code that was commented out

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ const App = () => {
   };
   const mapContainer = useRef(null);
   const map = useRef(null);
-  // const popupRef = useRef();
   const [lng, setLng] = useState(-83.75357);
   const [lat, setLat] = useState(27.791858);
   const [mouseLocation, setMouseLocation] = useState([]);
@@ -32,11 +31,6 @@ const App = () => {
   const getRandomColor = () => {
     return `#${Math.random().toString(16).substring(2, 8)}`;
   };
-  let worldviewFilter = [
-    "any",
-    ["==", "all", ["get", "worldview"]],
-    ["in", "US", ["get", "worldview"]],
-  ];
 
   const randomColors = getRandomColor();
   useEffect(() => {
@@ -48,22 +42,13 @@ const App = () => {
       zoom: zoom,
     });
 
-    // const popup = new mapboxgl.Popup({
-    //   closeButton: false,
-    // });
     map.current.on("load", () => {
       console.log("randomcolors: ", randomColors);
       map.current.addSource("counties", {
         type: "vector",
         url: "mapbox://mapbox.82pkq93d",
       });
-      // map.current.addSource("counties", {
-      //   type: "geojson",
-      //   data: {
-      //     type: "FeatureCollection",
-      //     features: [floridaData],
-      //   },
-      // });
+
       map.current.addLayer(
         {
           id: "counties",
@@ -73,37 +58,18 @@ const App = () => {
           paint: {
             "fill-outline-color": "rgba(93,0,213,0.8)",
             "fill-color": "gray",
-            // "fill-color": randomColors,
-            // "fill-color": [
-            //   "interpolate",
-            //   ["linear"],
-            //   "#F2F12D",
-            //   "#EED322",
-            //   "#E6B71E",
-            //   "#DA9C20",
-            //   "#CA8323",
-            //   "#B86B25",
-            //   "#A25626",
-            //   "#8B4225",
-            //   "#723122",
-            // ],
             "fill-opacity": 0.8,
           },
-          // filter: ["in", "COUNTY", ""],
         },
-        // map.current.setPaintProperty("counties", "fill-color", randomColors),
-        // This final argument indicates that we want to add the Boundaries layer
-        // before the `waterway-label` layer that is in the map from the Mapbox
-        // Light style. This ensures the admin polygons are rendered below any labels
+        // before the `building` layer that is in the map from the Mapbox
         "building"
-        // "road-label-simple"
       );
-      // map.current.on("load", () => {
+
       map.current.setPaintProperty("counties", "fill-color", {
         property: thresholds.property,
         stops: thresholds.stops,
       });
-      // });
+
       map.current.on("move", () => {
         setLng(map.current.getCenter().lng.toFixed(4));
         setLat(map.current.getCenter().lat.toFixed(4));
@@ -113,15 +79,13 @@ const App = () => {
         setPopupVisible(true);
       });
       map.current.on("mousemove", "counties", (e) => {
-        // setMouseLocation(map.current.features[0]);
         map.current.getCanvas().style.cursor = "pointer";
-        // console.log(e.features[0]);
+
         setCursorX(e.point.x);
         setCursorY(e.point.y);
-        // console.log(set);
+
         if (e.features.length > 0) {
           const feature = e.features[0];
-          // console.log(feature);
           const casesFound = floridaData.filter(
             (d) => d.county === feature.properties.COUNTY
           )[0];
@@ -136,40 +100,19 @@ const App = () => {
             e.lngLat.lat.toFixed(4),
           ];
           setMouseLocation(countyLngLat);
-          // const popupNode = document.createElement("div");
           setCounty(feature.properties.COUNTY);
-          // popup
-          //   .setLngLat(e.lngLat)
-          //   .setDOMContent(popupRef.current)
-          //   // .setText(feature.properties.COUNTY)
-          //   .addTo(map.current);
-          // console.log(e.point);
-          // console.log("X", cursorX);
         }
       });
-      // const Y = Event.pageY;
-      // console.log("Mouse Y: ", Y);
+
       map.current.on("click", "counties", (e) => {
         console.log("Event Data: ", e.features);
       });
       map.current.on("mouseleave", "counties", () => {
         map.current.getCanvas().style.cursor = "";
         setPopupVisible(false);
-        // setCases(null);
-        // popup.remove();
       });
-      // map.current.setPaintProperty("counties", "fill-color", randomColors);
     });
   });
-  // useEffect(() => {
-  //   map.current.setPaintProperty("counties", "fill-color", randomColors);
-  // }, [randomColors]);
-  const cursorPopupStyle = {
-    padding: "10px",
-    backgroundColor: "black",
-    color: "white",
-    position: "absolute",
-  };
 
   return (
     <div>
@@ -200,7 +143,6 @@ const App = () => {
           </div>
         </>
       )}
-      {/* <span>test</span> */}
       <div ref={mapContainer} className="map-container" />
     </div>
   );
@@ -214,15 +156,12 @@ const PopUp = ({ county, cursorX, cursorY, cases }) => {
     position: "absolute",
     padding: "10px",
     borderRadius: "10px",
-    // opacity: 0.5,
   };
   return (
     <div style={{ position: "relative" }}>
       <div className="popup" style={style}>
         <span>{county}</span>
-        {/* <span>Cases: {cases ? cases.cases : 0}</span> */}
         {cases && <span>Cases: {cases.cases}</span>}
-        {/* <span>Disease: {cases ? cases.disease : "No diseases"}</span> */}
         {cases && <span>Disease: {cases.disease}</span>}
       </div>
     </div>
@@ -230,11 +169,6 @@ const PopUp = ({ county, cursorX, cursorY, cases }) => {
 };
 
 const Legend = () => {
-  // [0, "#34dbe0"],
-  //     [10000, "#347ce0"],
-  //     [50000, "#9f37db"],
-  //     [100000, "#4d15a1"],
-  //     [200000, "#2c0566"],
   const legendItems = [
     { thresholdColor: "#34dbe0", numberOfPeople: 0 },
     { thresholdColor: "#347ce0", numberOfPeople: 10000 },
