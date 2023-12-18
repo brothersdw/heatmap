e2fe994ccb72f1a7003f298b5cac408f74f7b2d2
dbrothers <dbrothers@ruvos.com>
2023-12-18 17:10:46 -0500

    Cleaning up components adding some comments. More to come...

---
+++ src/components/county-boundary-map/index.js
@@ -0,0 +1,5 @@
+export { DiseaseDropDown } from "./disease-dropdown";
+export { MapPopup } from "./map-popup";
+export { Legend } from "./legend";
+export { HeatMap } from "./heat-map";
+export { DataBox } from "./data-box";
---
+++ src/components/county-boundary-map/map-popup.js
@@ -0,0 +1,22 @@
+export const MapPopup = ({ county, cursorX, cursorY, cases }) => {
+  const style = {
+    left: `${cursorX - (cases ? 80 : 60)}px`,
+    top: `${cursorY - (cases ? 80 : 45)}px`,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    position: "absolute",
+    padding: "10px",
+    borderRadius: "10px",
+    zIndex: 2,
+  };
+  return (
+    <div style={{ position: "relative" }}>
+      <div className="popup" style={style}>
+        <span>{county}</span>
+        {cases && <span>Cases: {cases.cases}</span>}
+        {cases && <span>Disease: {cases.disease}</span>}
+        {/* <span>Cases: {cases.cases}</span>
+          <span>Disease: {cases.disease}</span> */}
+      </div>
+    </div>
+  );
+};
