3a97c0b7c7a2e8275fe4336be0a9c18521f5da24
dbrothers <dbrothers@ruvos.com>
2024-01-03 11:03:20 -0500

    Added comment for popup component and removed unnecessary parent div for popup

--- src/components/heat-map/map-popup.js
+++ src/components/heat-map/map-popup.js
@@ -1,5 +1,6 @@
 export const MapPopup = ({ county, cursorX, cursorY, cases }) => {
   const style = {
+    // !IMPORTANT: The left and top is dynamic and based on the states of cursorX and cursorY which are set in the HeatMap component
     left: `${cursorX - (cases ? 80 : 60)}px`,
     top: `${cursorY - (cases ? 85 : 45)}px`,
     backgroundColor: "rgba(255, 255, 255, 0.8)",
@@ -9,12 +10,10 @@ export const MapPopup = ({ county, cursorX, cursorY, cases }) => {
     zIndex: 2,
   };
   return (
-    <div style={{ position: "relative" }}>
-      <div className="popup" style={style}>
-        <span>{county}</span>
-        {cases && <span>Cases: {cases.cases}</span>}
-        {cases && <span>Disease: {cases.disease}</span>}
-      </div>
+    <div className="popup" style={style}>
+      <span>{county}</span>
+      {cases && <span>Cases: {cases.cases}</span>}
+      {cases && <span>Disease: {cases.disease}</span>}
     </div>
   );
 };
